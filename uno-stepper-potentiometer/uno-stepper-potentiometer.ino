6fdbc18f7f4a547046e921b54ef08b8b2254744c
Tirta Rachman <tirtawening@gmail.com>
2019-10-14 23:57:20 -0400

    Add uno-stepper-potentiometer project

---
+++ uno-stepper-potentiometer/uno-stepper-potentiometer.ino
@@ -0,0 +1,98 @@
+#define STEPPER_PIN_1 9
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 11
+#define STEPPER_PIN_4 12
+#define POTENTIOMETER_PIN 0
+
+int stepNumber = 0;
+int sensorValue = 0;
+int delayValue = 0;
+
+void setup() {
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(POTENTIOMETER_PIN);
+  delayValue = map(sensorValue, 0, 1023, 0, 100 / 5); // lower resolution for smoothness
+  delayValue = delayValue * 5;
+
+  
+  if (delayValue < 2) {
+    delayValue = 2;
+  }
+
+//  Serial.print("delayValue = ");
+//  Serial.print(delayValue);
+//  Serial.print("\n");
+  
+  oneStep(true); 
+  delay(delayValue);
+}
+
+
+void oneStep(bool isClockwise) {
+  if(isClockwise) {
+    switch(stepNumber) {
+      case 0:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+    } 
+  } else {
+    switch(stepNumber) {
+      case 0:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+    } 
+  }
+  stepNumber++;
+  if(stepNumber > 3) {
+    stepNumber = 0;
+  }
+}
