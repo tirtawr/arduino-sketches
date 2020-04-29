0449d90aed4380e18eedcac79cdd8468adf9b2cd
Tirta Rachman <tirtawening@gmail.com>
2020-04-29 00:10:16 -0400

    update chord progression

--- joystick-chord-machine/joystick-chord-machine.ino
+++ joystick-chord-machine/joystick-chord-machine.ino
@@ -12,10 +12,11 @@ const int ledPin = 6;
 //int diminishedChord[] = {0, 3, 6};
 //int augmentedChord[] = {0, 4, 8};
 //int majorSeventhChord[] = {0, 4, 8, 11};
-int chordSize = 3;
-int chord[] = {0, 4, 7};
+//int majorEleventhChord[] = {0, 4, 7, 11, 14};
 
-// start with middle C:
+int chordSize = 5;
+int chord[] = {0, 4, 7, 11, 14};
+
 //int tonic = pitchC4;
 int tonic = 0;
 // note to play:
@@ -28,13 +29,14 @@ int lastbuttonDownState = HIGH;
 int lastbuttonLeftState = HIGH;
 int lastbuttonRightState = HIGH;
 unsigned int previousMillis;
-unsigned int interval = 100;
+unsigned int interval = 150;
 
 boolean isTonePlaying = false;
 int currentOffsetIndex = 0;
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(0));
   pinMode(buttonMainPin, INPUT_PULLUP);
   pinMode(buttonUpPin, INPUT_PULLUP);
   pinMode(buttonDownPin, INPUT_PULLUP);
@@ -45,11 +47,14 @@ void setup() {
 
 void loop() {
   checkButtonStates();
+  guardNoteTopThreshold();
+  guardNoteBottomThreshold();
+
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    if (digitalRead(buttonMainPin) == LOW) {
+    if (isPressed(buttonMainPin)) {
       playNote();
     }
   }
@@ -69,28 +74,24 @@ void onButtonMainReleased() {
 
 void onButtonUpPressed() {
   baseNote++;
-  guardNoteTopThreshold();
 }
 
 void onButtonUpReleased() { }
 
 void onButtonDownPressed() {
   baseNote--;
-  guardNoteBottomThreshold();
 }
 
 void onButtonDownReleased() { }
 
 void onButtonLeftPressed() {
   baseNote -= 12;
-  guardNoteBottomThreshold();
 }
 
 void onButtonLeftReleased() { }
 
 void onButtonRightPressed() {
   baseNote += 12;
-  guardNoteTopThreshold();
 }
 
 void onButtonRightReleased() { }
@@ -154,7 +155,15 @@ void checkButtonStates() {
 
 int getNextOffset(int arr[]) {
   currentOffsetIndex++;
-  if (currentOffsetIndex >= chordSize) currentOffsetIndex = 0;
+  if (currentOffsetIndex >= chordSize) {
+    currentOffsetIndex = 0;
+    int randomNumber = random(100);
+    if (randomNumber > 66) {
+      baseNote += random(8);
+    } else if (randomNumber < 33) {
+      baseNote -= random(8);
+    }
+  }
   return arr[currentOffsetIndex];
 }
 
@@ -172,11 +181,15 @@ void sendNoteOffEvent(byte note) {
 }
 
 void guardNoteBottomThreshold() {
-  if(baseNote < 0) baseNote = 0;
+  if (baseNote < 0) baseNote = 0;
 }
 
 void guardNoteTopThreshold() {
-  if(baseNote + 12 > 127) baseNote = 115;
+  if (baseNote + 12 > 127) baseNote = 115;
+}
+
+boolean isPressed(int pin) {
+  return digitalRead(pin) == LOW;
 }
 
 void debounce() {
